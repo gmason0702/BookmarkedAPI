71ec2a8288374cc7aaf88ec8a76ef02fc795afd9
G Mason <gmason0702@gmail.com>
2020-03-04 21:42:55 -0500

    fourthCommit

--- Bookmarked.Data/Book.cs
+++ Bookmarked.Data/Book.cs
@@ -8,5 +8,12 @@ namespace Bookmarked.Data
 {
     public class Book
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Author { get; set; }
+        public string Genre { get; set; }
+        public DateTimeOffset CreatedUtc { get; set; }
+        public DateTimeOffset PublishedDate { get; set; }
+        public ICollection<UserBookJoin> UserBookJoins { get; set; }
     }
 }
--- Bookmarked.Services/BookService.cs
+++ Bookmarked.Services/BookService.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Bookmarked.Data;
+using Bookmarked.Models;
+using BookmarkedAPI.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +11,90 @@ namespace Bookmarked.Services
 {
     public class BookService
     {
+        private readonly int _userId;
+        public BookService(int userId)
+        {
+            _userId = userId;
+        }
+        public bool CreateBook(BookCreate model)
+        {
+            var entity =
+                new Book()
+                {
+                    Name = model.Name,
+                    Author = model.Author,
+                    Genre = model.Genre,
+                    CreatedUtc = DateTimeOffset.Now
+                };
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.Books.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
+        }
+        public IEnumerable<BookListItem> GetBooks()
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query =
+                    ctx
+                    .Books
+                    .Where(e => e.Id == _userId)
+                    .Select(
+                        e =>
+                        new BookListItem
+                        {
+                            Id = e.Id,
+                            Name = e.Name,
+                            Author = e.Author,
+                            UserBookJoins = e.UserBookJoins
+
+                        }
+                    );
+                return query.ToArray();
+            }
+        }
+        public BookDetail GetBookByName(string name)
+        {
+            using (var ctx= new ApplicationDbContext())
+            {
+                var entity =
+                    ctx
+                        .Books
+                        .Single(e => e.Name == name);
+                return
+                    new BookDetail
+                    {
+                        Id = entity.Id,
+                        Name = entity.Name,
+                        Author = entity.Author,
+                        Genre = entity.Genre,
+                        CreatedUtc = entity.CreatedUtc,
+                        PublishedDate = entity.PublishedDate,
+                        UserBookJoins = entity.UserBookJoins
+                    };
+            }
+        }
+        public BookDetail GetBookByGenre(string genre)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity =
+                    ctx
+                    .Books
+                    .Single(e => e.Genre == genre);
+                return
+                    new BookDetail
+                    {
+                        Id = entity.Id,
+                        Name = entity.Name,
+                        Author = entity.Author,
+                        Genre = entity.Genre,
+                        CreatedUtc = entity.CreatedUtc,
+                        PublishedDate = entity.PublishedDate,
+                        UserBookJoins = entity.UserBookJoins
+                    };
+            }
+        }
     }
 }
