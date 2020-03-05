08b3ca6a3eb3e3eef6e00fcbe294978004c05a7b
G Mason <gmason0702@gmail.com>
2020-03-05 10:24:06 -0500

    fifthCommit

--- Bookmarked.Models/BookListItem.cs
+++ Bookmarked.Models/BookListItem.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Bookmarked.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,9 @@ namespace Bookmarked.Models
 {
     public class BookListItem
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Author { get; set; }
+        public ICollection<UserBookJoin> UserBookJoins { get; set; }
     }
 }
