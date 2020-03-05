4b349e2c39628542fecaec355164b67ce5d24f5f
Andrea-Kimball <aundreya.7@gmail.com>
2020-03-05 10:24:09 -0500

    Added joining tables

--- Bookmarked.Data/UserBookJoin.cs
+++ Bookmarked.Data/UserBookJoin.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,6 +9,16 @@ using System.Threading.Tasks;
 namespace Bookmarked.Data
 {
     public class UserBookJoin
-    {
+    {       
+        [Key]
+        public int Id { get; set; }
+        [ForeignKey("ReaderId")]
+        public int ReaderId { get; set; }
+        //navigation property-return type is the class that the Foreign key is relating to         
+        public Reader Reader { get; set; }
+        public int BookId { get; set; }
+        [ForeignKey("BookId")]
+        public Book Book { get; set; }
+        public DateTimeOffset CreatedUtc { get; set; }
     }
 }
