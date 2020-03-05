336bbd2739289d0df15a66db83452e037d100f67
Alecyancey <59926558+Alecyancey@users.noreply.github.com>
2020-03-05 10:35:11 -0500

    Added BookClub classes, models, services, controllers

--- Bookmarked.Services/BookClubService.cs
+++ Bookmarked.Services/BookClubService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Bookmarked.Data;
+using Bookmarked.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,23 @@ namespace Bookmarked.Services
 {
     public class BookClubService
     {
+        private readonly int _userId;
+        public BookClubService(int userId)
+        {
+            _userId = userId;
+        }
+        public bool CreateBookClub(BookClubCreate model)
+        {
+            var entity =
+                new BookClub()
+                {
+                };
+
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.Notes.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
+        }
     }
 }
