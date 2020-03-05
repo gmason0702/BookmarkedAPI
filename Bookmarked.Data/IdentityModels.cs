d088934ebe3a54811238d93c8830b3d9ad9a2a6f
Alecyancey <59926558+Alecyancey@users.noreply.github.com>
2020-03-05 10:56:57 -0500

    BookClub

--- Bookmarked.Data/IdentityModels.cs
+++ Bookmarked.Data/IdentityModels.cs
@@ -1,5 +1,7 @@
-﻿using System.Security.Claims;
+﻿using System.Data.Entity;
+using System.Security.Claims;
 using System.Threading.Tasks;
+using Bookmarked.Data;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.AspNet.Identity.Owin;
@@ -29,5 +31,6 @@ namespace BookmarkedAPI.Data
         {
             return new ApplicationDbContext();
         }
+        public DbSet<BookClub> BookClubs { get; set; }
     }
 }
--- BookmarkedAPI/Controllers/BookClubController.cs
+++ BookmarkedAPI/Controllers/BookClubController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Bookmarked.Models;
+using Bookmarked.Services;
+using Microsoft.AspNet.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -9,5 +12,35 @@ namespace BookmarkedAPI.Controllers
 {
     public class BookClubController : ApiController
     {
+        public IHttpActionResult Get()
+        {
+            BookClubService bookClubService = CreateBookClubService();
+            var notes = bookClubService.GetBookClubs();
+            return Ok(notes);
+        }
+        public IHttpActionResult Get(int id)
+        {
+            BookClubService noteService = CreateBookClubService();
+            var note = noteService.GetBookClubById(id);
+            return Ok(note);
+        }
+        public IHttpActionResult Post(BookClubCreate note)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = CreateBookClubService();
+
+            if (!service.CreateBookClub(note))
+                return InternalServerError();
+
+            return Ok();
+        }
+        private BookClubService CreateBookClubService()
+        {
+            var userId = Convert.ToInt32(User.Identity.GetUserId());
+            var noteService = new BookClubService(userId);
+            return noteService;
+        }
     }
 }
