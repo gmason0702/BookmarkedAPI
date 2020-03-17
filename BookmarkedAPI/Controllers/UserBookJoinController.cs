1c2e1a0f3e56a4d9a5ee2dd2fc04640050f93915
Nick Wehner <58861995+nrwehner@users.noreply.github.com>
2020-03-17 13:24:56 -0400

    Update UserBookJoinController.cs

--- BookmarkedAPI/Controllers/UserBookJoinController.cs
+++ BookmarkedAPI/Controllers/UserBookJoinController.cs
@@ -41,13 +41,6 @@ namespace BookmarkedAPI.Controllers
             return Ok(bookJoin);
         }
 
-        public IHttpActionResult GetAllUserRatings(string username)
-        {
-            UserBookJoinService userBookJoinService = CreateUserBookJoinService();
-            var bookJoin = userBookJoinService.GetAllRatingsByUser(username);
-            return Ok(bookJoin);
-
-        }
 
         public IHttpActionResult GetAllUserRatings(string user)
         {
